6edce18423299027f59342c76c65ef5fa13c97d6
kjj6198 <kjj6198@gmail.com>
2020-03-23 10:56:27 +0000

    adjust shape_of_you

--- shape_of_you.rb
+++ shape_of_you.rb
@@ -4,13 +4,15 @@ define :create_my_beat do |tempo|
   return ring(*tempo.split("").map { |beat| beat == "*" })
 end
 
-melody = ring([:a5, :f5], [:c6, :f5], [:a5, :f5], [:d5, :a5], [:a5, :c6], [:d5, :a5], [:f5, :a5], [:a5, :c6], [:f5, :a5], [:g5,:b5], [:f5, :a5], [:e5, :g5])
+melody = ring(:a5, :c6, :a5, :a5,  :c6,:a5, :a5, :c6, :a5, :b5, :a5, :g5)
 chords = ring(
   chord(:a4, :minor),
   chord(:d4, :minor),
   chord(:f4, :major),
   chord(:g4, :major)
 )
+
+ohohoh = ring(:a4, :c5, :e5)
 
 bassline = ring(
   :a2, :a2, :a2,
@@ -23,18 +25,20 @@ bassline = ring(
 tempo = create_my_beat "*--*--*-"
 amps = ring([0.8, 0.2, 0.2, 0.8, 0.2, 0.2, 0.8, 0.2])
 
-snare_tempo = create_my_beat "--**--*--"
+
+snare_tempo = create_my_beat "--**--*-"
+tom_tempo = create_my_beat "*---*-*--"
+
 live_loop :snare do
-  
-  sample :drum_cymbal_closed, amp: [0.8, 0.2].choose() if snare_tempo.tick
+  sample :drum_tom_mid_hard if tom_tempo.tick
+  sample :drum_cymbal_closed, amp: [0.8, 0.2].choose() if snare_tempo.look
   sleep 0.125
-  
 end
 
 
 live_loop :kick do
   with_fx :sound_out_stereo do
-    sample :bd_tek, amp: 1.2 if tempo.tick
+    sample :bd_tek, amp: 2 if tempo.tick
     sleep 0.125
   end
 end
@@ -49,15 +53,22 @@ end
 
 live_loop :baseline do
   with_fx :reverb, room: 0.5 do
-    
+    use_synth :sine
     if tempo.tick
-      play melody.tick(:melody), release: 0.1
+      play melody.tick(:melody), release: 0.12,amp: 3
       if look(:melody) % 3 === 0
-        play_chord chords.tick(:chord).take(2), release: 0.1
+        play_chord chords.tick(:chord).take(2), release: 0.12, amp: 3
       else
-        play_chord chords.look(:chord).take(2), release: 0.1
+        play_chord chords.look(:chord).take(2), release: 0.12, amp: 3
       end
     end
     sleep 0.125
   end
 end
+
+live_loop :sample do
+  sample :loop_industrial, beat_stretch: 2
+  sleep 3.75
+end
+
+
